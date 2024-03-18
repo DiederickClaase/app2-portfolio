2654e59a2012e9e1ec5a76d06a24fc68b3facd95
diederick <diederickc@gmail.com>
2024-03-18 19:21:25 +0200

    Day22: Render App titles in two colm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -10,9 +11,19 @@ with col1:
 with col2:
     st.title('Diederick Claase')
     content = """This is where I need to write something about myself"""
-
     st.info(content)
 
 content2 = """Below you can find some of my applications that I have built in Python. Please feel free to look around"""
 st.write(content2)
 
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
