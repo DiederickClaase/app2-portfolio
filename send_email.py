c25380805976ff3abafdd063fff453c8ce758c50
diederick <diederickc@gmail.com>
2024-03-18 23:36:50 +0200

    Day23: send email

---
+++ send_email.py
@@ -0,0 +1,17 @@
+import smtplib
+import ssl
+
+host = 'smtp.gmail.com'
+port = 465
+
+username = "diederickc@gmail.com"
+password = "lasv urpu neos nofg"
+
+receiver = 'diederickc@gmail.com'
+context = ssl.create_default_context()
+
+message = """How are you"""
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(username, password)
+    server.sendmail(username, receiver, message)
+
